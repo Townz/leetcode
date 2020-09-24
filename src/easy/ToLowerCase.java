640c4ee8f70ff60bc126de5e9651e319911956e9
Townz <townz@live.com>
2020-09-23 19:50:44 -0600

    Adding comments to previous solutions

--- src/easy/ToLowerCase.java
+++ src/easy/ToLowerCase.java
@@ -6,3 +6,18 @@ public class ToLowerCase
         return str.toLowerCase();
     }
 }
+
+/* sImplement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+Example 1:
+Input: "Hello"
+Output: "hello"
+
+Example 2:
+Input: "here"
+Output: "here"
+
+Example 3:
+Input: "LOVELY"
+Output: "lovely"
+ */
